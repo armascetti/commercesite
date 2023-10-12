a03ca4405fdacaa75087e541d07072ca03a27b00
Mandy_Mascetti <armascetti@gmail.com>
2023-10-12 12:07:17 -0400

    display content

--- src/App.jsx
+++ src/App.jsx
@@ -8,17 +8,17 @@ import NavBar from './components/NavBar'
 
 function App() {
   return (
-    <Router> 
+    <Router>
       <NavBar />
-    <div className='max-w-screen-md mx-auto pt-20'>
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/about' element={<About />} />
-      <Route path='/articles-list' element={<ArticlesList />} />
-      <Route path='/article' element={<Article />} />
-    </Routes>
-    </div>
-      </Router> 
+      <div className='max-w-screen-md mx-auto pt-20'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/articles-list' element={<ArticlesList />} />
+          <Route path='/article/:name' element={<Article />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <div className='mb-20'>
       <h1 className='sm:text-4xl text-2xl font-bold my-6 text-gray-900'> Hellloooo</h1>
-      <p className='mx-auto leading-relaxed text-base mb-4'> 
+      <p className='mx-auto leading-relaxed text-base mb-4'>
         what it do
       </p>
     </div>
